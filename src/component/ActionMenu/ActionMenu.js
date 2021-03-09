653c148bb6ca4c42b007dbae052a97a4c95906c4
Cesar Cordoba <cordobacesar29@gmail.com>
2021-03-09 17:22:35 -0300

    estilos css al componente ActionMenu

---
+++ src/component/ActionMenu/ActionMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ActionMenu.css';
+
+export const ActionMenu = () => {
+    return (
+        <div className="actions-menu">
+            <h1 className="page-title">Mascotas</h1>
+            <button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Nueva
+            </button>
+            <div id="actions-menu"></div>
+        </div>
+    )
+}
