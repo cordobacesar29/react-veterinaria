59aa92a3e83ffd488f4ab1bd530ff41332992d90
Cesar Cordoba <cordobacesar29@gmail.com>
2021-03-09 22:16:15 -0300

    mostrar y ocultar alert con useState

--- src/component/ActionMenu/ActionMenu.js
+++ src/component/ActionMenu/ActionMenu.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ActionMenu.css';
 import { Alert } from '../Alert/Alert';
 
 export const ActionMenu = () => {
+    const [mostrarAlerta, setMostrarAlerta] = useState(false);
     return (
         <div className="actions-menu">
-            <h1 className="page-title">Mascotas</h1>
-            <button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Nueva
-            </button>
-            <div id="actions-menu"></div>
-            <Alert/>
+            <div className="actions-menu-content">
+                <div>
+                    <h1 className="page-title">Mascotas</h1>
+                    <button 
+                        type="button" 
+                        className="btn btn-dark" 
+                        data-bs-toggle="modal" 
+                        data-bs-target="#exampleModal"
+                        onClick = {()=>setMostrarAlerta(!mostrarAlerta)}
+                    >
+                        Nueva
+                    </button>
+                </div>
+                { mostrarAlerta && <Alert/> }
+            </div>            
         </div>
     )
 }
