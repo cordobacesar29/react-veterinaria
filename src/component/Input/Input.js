06ea50eaa65e74c404adff0e634c93c806f3dfa6
Cesar Cordoba <cordobacesar29@gmail.com>
2021-03-22 09:46:06 -0300

    arreglo falla return

--- src/component/Input/Input.js
+++ src/component/Input/Input.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Input.css';
 
-export const Input = (tipo = 'text', nombreCampo) => {
-    <input 
-        type={tipo} 
-        id={nombreCampo}
-        name={nombreCampo}
-        className="form-control" 
-        placeholder={nombreCampo}
-        aria-label={nombreCampo}
-    />
+export const Input = ({ tipo = 'text', nombreCampo }) => {
+    return( 
+        <input 
+            type={tipo} 
+            id={nombreCampo}
+            name={nombreCampo}
+            className="form-control" 
+            placeholder={nombreCampo}
+            aria-label={nombreCampo}
+        />
+    )
 }
